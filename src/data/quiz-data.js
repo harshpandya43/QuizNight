7ee75ee73724400b2f47d1f0d37f591e4fa84c28
Harsh Pandya <pandyaharsh43@gmail.com>
2020-11-21 21:12:49 -0600

    first commit

---
+++ src/data/quiz-data.js
@@ -0,0 +1,146 @@
+import React from 'react';
+
+const QUESTION_DATA = [
+  {
+    question: <span>Where would I like to go on a vacation with you?</span>,
+    answers: [
+      <span>Mountains</span>,
+      <span>Beach</span>,
+      <span>Some village</span>,
+      <span>Stay at home and have sex.</span>
+    ],
+    correct: 1
+  },
+  {
+    question: <span>Who is CEO of Tesla?</span>,
+    answers: [
+      <span>Jeff Bezos</span>,
+      <span>Elon Musk</span>,
+      <span>Bill Gates</span>,
+      <span>Donald Trump</span>
+    ],
+    correct: 1
+  },
+  {
+    question: <span>Which date night would appeal me the most?</span>,
+    answers: [
+      <span>Going out for drink</span>,
+      <span>Going to the theatre</span>,
+      <span>Ordering Pizza, Watching movie at home and having sex</span>,
+      <span>All of the above</span>
+    ],
+    correct: 3
+  },
+  {
+    question: <span>Out of these which meal I would prefer the most?</span>,
+    answers: [
+      <span>Subway Sandwich</span>,
+      <span>Burger</span>,
+      <span>Tacos</span>,
+      <span>Salad</span>
+    ],
+    correct: 1
+  },
+  {
+    question: <span>Which vacation destination would I pick?</span>,
+    answers: [
+      <span>Hawaii</span>,
+      <span>Disney World</span>,
+      <span>Spain</span>,
+      <span>Paris</span>
+    ],
+    correct: 0
+  },
+  {
+    question: <span>Who is my most favorite superhero?</span>,
+    answers: [
+      <span>Superman</span>,
+      <span>Batman</span>,
+      <span>Hanuman</span>,
+      <span>Spiderman</span>
+    ],
+    correct: 1
+  },
+  {
+    question: <span>What was my ambition to become when I was a kid?</span>,
+    answers: [
+      <span>Doctor</span>,
+      <span>Lawyer</span>,
+      <span>President of India</span>,
+      <span>Judge</span>
+    ],
+    correct: 2
+  },
+  {
+    question: <span>Out of these which is the thing do I love doing the most to you in bed?</span>,
+    answers: [
+      <span>Cuddle</span>,
+      <span>Kiss</span>,
+      <span>Tickle</span>,
+      <span>Sleep with my hand on your stomach</span>
+    ],
+    correct: 3
+  },
+  {
+    question: <span>Which facial feature of yours I love the most?</span>,
+    answers: [
+      <span>Your cute nose</span>,
+      <span>Sexy Eyes</span>,
+      <span>Luscious Lips</span>,
+      <span>Cute Smile</span>
+    ],
+    correct: 1
+  },
+  {
+    question: <span>Which activity I want to do with you?</span>,
+    answers: [
+      <span>Paint together</span>,
+      <span>Karaoke</span>,
+      <span>Cook together</span>,
+      <span>Play Dota</span>
+    ],
+    correct: 0
+  },
+  {
+    question: <span>Name the first movie we saw together?</span>,
+    answers: [
+      <span>Love Aaj Kal</span>,
+      <span>Me Before You</span>,
+      <span>Bhoot</span>,
+      <span>Shubh Mangal Zyada Savdhaan</span>
+    ],
+    correct: 3
+  },
+  {
+    question: <span>Other than 69 which other sexual position I wanna do with you the most?</span>,
+    answers: [
+      <span>Doggystyle</span>,
+      <span>Cowgirl</span>,
+      <span>The Standing Ovation</span>,
+      <span>Spooning</span>
+    ],
+    correct: 0
+  },
+  {
+    question: <span>'What kind of traveler am I?</span>,
+    answers: [
+      <span>Planner, who likes to set an itnerary for everything</span>,
+      <span>Spontaneous</span>,
+      <span>I prefer to stay in hotel and sleep</span>,
+      <span>I don't like to travel</span>
+    ],
+    correct: 0
+  },
+  {
+    question: <span>The movie which I want to watch with you when we would be together</span>,
+    answers: [
+      <span>Sarfarosh</span>,
+      <span>Andaaz Apna Apna</span>,
+      <span>Nayak: The Real Hero</span>,
+      <span>Bahubali: The Beginning and The Conclusion</span>
+    ],
+    correct: 1
+  }
+];
+
+export default QUESTION_DATA;
